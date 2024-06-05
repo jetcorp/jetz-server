3e57df08b581a05992e59d58032d4ff6ce2e3e38
k1mjunyoung <me@kimjunyoung.com>
2024-06-05 16:48:21 +0900

    Initial commit

---
+++ src/main/java/jetz/server/JetzServerApplication.java
@@ -0,0 +1,13 @@
+package jetz.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetzServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetzServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jetz/server/JetzServerApplicationTests.java
@@ -0,0 +1,13 @@
+package jetz.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetzServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
