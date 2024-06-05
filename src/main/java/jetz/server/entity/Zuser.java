5b315fcb8059e84f962a07682e8d4718ebd19cb0
k1mjunyoung <me@kimjunyoung.com>
2024-06-05 18:13:30 +0900

    [Z-1] 유저 도메인 개발

---
+++ src/main/java/jetz/server/entity/Zuser.java
@@ -0,0 +1,31 @@
+package jetz.server.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Zuser {
+    @Id
+    @Column(columnDefinition = "BIGINT(20) NOT NULL UNIQUE KEY auto_increment")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String zname;
+
+    @Column(nullable = false)
+    private LocalDateTime regDateTime;
+}
