84a1490bad119bf3bb6fdd1683c68185d658eb51
k1mjunyoung <me@kimjunyoung.com>
2024-06-07 14:39:48 +0900

    [Z-3] 블로그 도메인 개발
    블로그 관련 엔티티 구현

---
+++ src/main/java/jetz/server/entity/Post.java
@@ -0,0 +1,44 @@
+package jetz.server.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long index;
+
+    @ManyToOne
+    private Blog blog;
+
+    @Column(nullable = false)
+    private String subject;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime regDateTime;
+
+    private LocalDateTime modDateTime;
+
+    private Long likes;
+
+    private Long hits;
+
+    private String isPublic;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
+    private List<Reply> replyList;
+}
