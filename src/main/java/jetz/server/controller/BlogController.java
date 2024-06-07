50966617d7cdf9abddae389155817a5c09930215
k1mjunyoung <me@kimjunyoung.com>
2024-06-07 15:13:51 +0900

    [Z-3] 블로그 도메인 개발

---
+++ src/main/java/jetz/server/controller/BlogController.java
@@ -0,0 +1,16 @@
+package jetz.server.controller;
+
+import jetz.server.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogController {
+    @GetMapping("/@{zname}")
+    public ResponseEntity blog(@PathVariable String zname) {
+        return new ResponseEntity<>(zname, HttpStatus.OK);
+    }
+}
