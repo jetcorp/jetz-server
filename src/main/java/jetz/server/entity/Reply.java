20daa714de68560f1ba5408187c5bcdce5f36d72
k1mjunyoung <me@kimjunyoung.com>
2024-06-10 12:34:24 +0900

    [Z-5] 인증 API 개발
    fix: 테이블 생성 안되는 에러 해결

--- src/main/java/jetz/server/entity/Reply.java
+++ src/main/java/jetz/server/entity/Reply.java
@@ -29,7 +29,7 @@ public class Reply {
     @JoinColumn(name = "zname")
     private Zuser zname;
 
-    @Column(columnDefinition = "TEXT")
+    @Column
     private String content;
 
     private LocalDateTime regDateTime;
